ba3b0c4f6a708ffbaa42b94de75bd9a223b108f4
Kata12344 <kataszundi@gmail.com>
2022-06-04 15:15:56 +0200

    Lada metódusok

--- src/modell/Lada.java
+++ src/modell/Lada.java
@@ -12,6 +12,23 @@ public class Lada {
         this.vanKincs = vanKincs;
     }
     
+    public void AranyLada(){
+        szin = "arany";
+        felirat = "Én rejtem a kincset";
+        vanKincs = false;
+    }
+    
+    public void EzustLada(){
+        szin = "ezüst";
+        felirat = "Nem én rejtem a kincset";
+        vanKincs = true;
+    }
+    
+    public void BronzLada(){
+        szin = "bronz";
+        felirat = "Az arany hazudik";
+        vanKincs = false;
+    }
 
     public String getSzin() {
         return szin;
