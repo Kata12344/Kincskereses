4fc963bd2853a6dfad3c2e2209d7afcb8a1655b8
Kata12344 <kataszundi@gmail.com>
2022-06-04 14:59:57 +0200

    Lada osztály: konstruktor, getter

--- src/modell/Lada.java
+++ src/modell/Lada.java
@@ -1,13 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package modell;
 
-/**
- *
- * @author User
- */
 public class Lada {
+    private String szin;
+    private String felirat;
+    private boolean vanKincs;
+
+    public Lada(String szin, String felirat, boolean vanKincs) {
+        this.szin = szin;
+        this.felirat = felirat;
+        this.vanKincs = vanKincs;
+    }
+    
+
+    public String getSzin() {
+        return szin;
+    }
+
+    public String getFelirat() {
+        return felirat;
+    }
+
+    public boolean isVanKincs() {
+        return vanKincs;
+    }
+    
     
 }
