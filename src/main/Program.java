3833ce9e5c582ba7abd98ee2c8ec297d42dcca26
Kata12344 <kataszundi@gmail.com>
2022-06-06 23:46:06 +0200

    gui: klikkre kép csere

--- src/main/Program.java
+++ src/main/Program.java
@@ -1,11 +1,13 @@
 
 package main;
 
+import nezet.KincskeresesGUI;
+
 
 public class Program {
 
     public static void main(String[] args) {
-        
+        new KincskeresesGUI().setVisible(true);
     }
     
 }
--- src/modell/Lada.java
+++ src/modell/Lada.java
@@ -5,6 +5,8 @@ public class Lada {
     private String szin;
     private String felirat;
     private boolean vanKincs;
+    
+    
 
     public Lada(String szin, String felirat, boolean vanKincs) {
         this.szin = szin;
@@ -12,23 +14,23 @@ public class Lada {
         this.vanKincs = vanKincs;
     }
     
-    public void AranyLada(){
-        szin = "arany";
-        felirat = "Én rejtem a kincset";
-        vanKincs = false;
-    }
-    
-    public void EzustLada(){
-        szin = "ezüst";
-        felirat = "Nem én rejtem a kincset";
-        vanKincs = true;
-    }
-    
-    public void BronzLada(){
-        szin = "bronz";
-        felirat = "Az arany hazudik";
-        vanKincs = false;
-    }
+//    public void AranyLada(){
+//        szin = "arany";
+//        felirat = "Én rejtem a kincset";
+//        vanKincs = false;
+//    }
+//    
+//    public void EzustLada(){
+//        szin = "ezüst";
+//        felirat = "Nem én rejtem a kincset";
+//        vanKincs = true;
+//    }
+//    
+//    public void BronzLada(){
+//        szin = "bronz";
+//        felirat = "Az arany hazudik";
+//        vanKincs = false;
+//    }
 
     public String getSzin() {
         return szin;
